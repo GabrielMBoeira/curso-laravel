306b5e6539138e3458d93e8b1446e7f294b413f4
Gabriel <gboeira777@gmail.com>
2021-06-03 10:00:54 -0300

    Seeders no Laravel

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; //Importar aqui
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Gabriel',
+            'email' => 'gabrielmboeira@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+    }
+}
